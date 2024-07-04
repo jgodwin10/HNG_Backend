88e27fa601c56b80346981f4a5ef0a84ca5edad7
jgodwin10 <easypeasy725@gmail.com>
2024-07-04 13:09:24 +0100

    Final Commit

--- Stage_01/weatherProject/urls.py
+++ Stage_01/weatherProject/urls.py
@@ -20,5 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.userip)
+    path('', views.userip),
+    path('api/hello/', views.userip)
 ]
--- Stage_01/weatherProject/views.py
+++ Stage_01/weatherProject/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import HttpResponse
+from django.http import JsonResponse
+import requests, json
+
+
 
 def userip(request):
+    name = request.GET['visitor_name']
     user_ip = request.META.get('HTTP_X_FORWARDED_FOR')
     if user_ip is not None:
         ip = user_ip.split(',')[0]
     else:
         ip = request.META.get('REMOTE_ADDR')
+    
+    payload = {'ip': ip, 'format': 'json'}
+    api_result = requests.get('https://api.ip2location.io/', params=payload)
+    city = api_result.json()
+    cityName = city['city_name']
 
-    return HttpResponse("Welcome user: {}".format(ip))
+    weather = requests.get('https://api.openweathermap.org/data/2.5/weather?q={}&appid=3b6cb4536a3f0c99e3d357906ad951f9'.format(cityName))
+    temps = weather.json()
+   
+
+    Temps = temps['main']['temp']
+
+    data = {
+        "client_ip": ip,
+        "location": cityName,
+        "greeting": f"Hello, {name}!, the temperature is {Temps} degrees celcius in {cityName}"
+    }
+    
+
+    return JsonResponse(data)
